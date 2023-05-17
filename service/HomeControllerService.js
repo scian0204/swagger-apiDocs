1b394235747a864b3ccf8b69bf179f4bd2b99aa5
scian0204 <scian0204@gmail.com>
2023-05-17 12:38:27 +0900

    fix error

---
+++ service/HomeControllerService.js
@@ -0,0 +1,19 @@
+'use strict';
+
+
+/**
+ *
+ * returns String
+ **/
+exports.test = function() {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    examples['application/json'] = "";
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+}
+
